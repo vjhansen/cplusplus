b69ce74a0beb65d0204e8a9b8e0c4a8b2b45d052
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-02 22:12:01 +0200

    Create w01_partB.cpp

---
+++ w01_partB.cpp
@@ -0,0 +1,42 @@
+/*
+ Part B
+ Create a program that reads a text string with two numbers and a operator and prints the result.
+ 
+ This text should be interpreted by the program.
+ 
+ Enter two numbers with an operator between> 10 + 15
+ The answer to 10 + 15 is 25.
+ Enter two numbers with an operator between> 10+15
+ The answer to 10 + 15 is 25.
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    
+    int num1, num2 = 0;
+    string str, str2 = "";
+    cout << "Enter two numbers with an operator between: "<< flush;
+    getline(cin, str);
+    
+    int len = (int)str.length();
+    for (int i = 0; i < len; i++) {
+        if (str[i] == ' ' )
+        {
+            for (int j = i; j < len; j++)
+                str[j] = str[j+1];
+            len--;
+        }
+    }
+    
+    char plus;
+    istringstream iss(str);
+    iss >> num1 >> plus >> num2;
+    
+    cout <<endl<< "The answer to " << num1 << '+' << num2 << " is " << num1 + num2<< endl;
+    return 0;
+}
