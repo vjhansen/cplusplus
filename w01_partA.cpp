045c3cad51d3896b38ade719d9cb69880e74b0cd
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-03 18:09:05 +0200

    Update w01_partA.cpp

--- w01_partA.cpp
+++ w01_partA.cpp
@@ -1,12 +1,12 @@
 /*
  Part A
  Create a program that reads two numbers and a operator and prints the result.
-
+ 
  Example (The text at the back> in each line is read during execution):
-     Numbers 1> 20
-     Operator> *
-     Numbers 2> 5
-     The answer of 20 * 5 is 100.
+ Numbers 1> 20
+ Operator> *
+ Numbers 2> 5
+ The answer of 20 * 5 is 100.
  */
 
 #include <iostream>
@@ -22,10 +22,38 @@ int main() {
     cin >> op;
     cout << "Number 2: " << endl;
     cin >> num2;
-    if (op == '*') {
-         cout << "The answer of " << num1 << op << num2 << " is " << num1*num2 << endl;
-    }
-    // and so on for +, -, /
     
+    if (!cin){
+        cout << "Numbers please.." << endl;
+        cin.clear();
+    }
+    
+    switch (op) {
+        case '*':
+            cout << "The answer of " << num1 << op << num2 << " is " << num1*num2 << endl;
+            break;
+            
+        case '+':
+            cout << "The answer of " << num1 << op << num2 << " is " << num1+num2 << endl;
+            break;
+            
+        case '-':
+            cout << "The answer of " << num1 << op << num2 << " is " << num1-num2 << endl;
+            break;
+            
+        case '/':
+            if (num2==0) {
+                cout << "Bad boy!" << endl;
+            }
+            else
+            cout << "The answer of " << num1 << op << num2 << " is " << num1/num2 << endl;
+            break;
+            
+        case '%':
+             cout << "The answer of " << num1 << op << num2 << " is " << num1%num2 << endl;
+             break;        
+        default:
+            break;
+    }
     return 0;
 }
