2272391320e3a5a6c49e37083b81f07c3da5f492
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-03 18:16:37 +0200

    Update w01_partB.cpp

--- w01_partB.cpp
+++ w01_partB.cpp
@@ -9,43 +9,44 @@
  Enter two numbers with an operator between> 10+15
  The answer to 10 + 15 is 25.
  
- 
- Error conditions must be handled. For example, entering an illegal operator or letters and not numbers.
-
-
- 
- 
  */
 
 #include <iostream>
-#include <string>
-#include <sstream>
 
 using namespace std;
 
 int main() {
     
     int num1, num2 = 0;
-    string str = "";
+    char op;
     cout << "Enter two numbers with an operator between: "<< flush;
-    getline(cin, str);
+    cin >> num1 >> op >> num2;
     
-    int len = (int)str.length();
-    for (int i = 0; i < len; i++) {
-        if (str[i] == ' ' )
-        {
-            for (int j = i; j < len; j++)
-                str[j] = str[j+1];
-            len--;
-        }
+    if (!cin) {
+        cout << "Only numbers please"<<endl;
     }
     
-    char plus;
-    istringstream iss(str);
-    iss >> num1 >> plus >> num2;
+    switch (op) {
+        case '+':
+            cout <<endl<< "The answer to " << num1 << op << num2 << " is " << num1 + num2<< endl;
+            break;
+        case '-':
+            cout <<endl<< "The answer to " << num1 << op << num2 << " is " << num1 - num2<< endl;
+            break;
+        case '*':
+            cout <<endl<< "The answer to " << num1 << op << num2 << " is " << num1 * num2<< endl;
+            break;
+        case '/':
+            if (num2 == 0) {
+                cout << "сука блять";
+            }
+            else
+                cout <<endl<< "The answer to " << num1 << op << num2 << " is " << num1 * num2<< endl;
+            break;
+            
+        default:
+            break;
+    }
     
- //use switch..case for -,/,*
- 
-    cout <<endl<< "The answer to " << num1 << '+' << num2 << " is " << num1 + num2<< endl;
     return 0;
 }
