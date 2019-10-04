6084744ac9db04ff818d705f6309f01dcd353af1
Victor Hansen <0vjhansen@gmail.com>
2019-10-04 13:44:02 +0200

    new

--- tsp_ass1/tsp_ass1/main.cpp
+++ tsp_ass1/tsp_ass1/main.cpp
@@ -2,16 +2,8 @@
 //  Victor
 
 
-/*
- - Use a Point class to represent the individual 3D points in the inputs.
- - pass const-references as arguments to the functions specified below; this will require you to update your Point class with const specifications where required.
- */
-
 // - 1. definer hvor mange byer vi skal besøke
-// - 2. angi posisjon for hver by.
-
-
-
+// - 2. angi posisjon (x,y,z) for hver by.
 
 
 #include <iostream>
@@ -19,11 +11,33 @@
 #include <cmath>
 #include <algorithm>
 #include <iterator>
-
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-
-
+    
+    // define vector of size 3 for holding xyz-coordinates
+    std::vector<int> v(3);
+    v.clear();
+    std::cout << "Enter numbers: ('Q' to quit)"<< std::endl;
+    
+int num;
+while (cin) {
+    std::cin >> num;
+    if (!cin) {
+        break;
+    }
+    v.push_back(num);
+    
+    if (v.size() > 3){
+        v.pop_back();  // use pop.back() to remove last item of vector
+    }
+}
+    
+    std::cout << "you entered " << v.size() << endl;
+    
+    for (int i =0; i<v.size(); i++) {
+        cout << v[i] << endl;
+    }
+    
     return 0;
 }
