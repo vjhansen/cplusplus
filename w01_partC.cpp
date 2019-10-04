d32f3799944975a5dcbf3400d8f289bf0c5cbc22
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-04 16:38:39 +0200

    Update w01_partC.cpp

--- w01_partC.cpp
+++ w01_partC.cpp
@@ -2,112 +2,99 @@
  Part C
  Create a program that reads a text string with multiple numbers and operators and prints the result. This text should be interpreted by the program.
  
- 
- Error conditions must be handled. For example, entering an illegal operator or letters and not numbers.
- 
  Example (The text at the back of the > is read during execution):
  Enter numbers with an operator between> 2 + 3 * 5
- The answer to 2 + 3 * 5 is 17.
+ The answer to 2 + (3 * 5) is 17.
  */
 
 #include <iostream>
 #include <string>
-#include <sstream>
-#define div /
 
 class calculator {
     
 public:
-    double num1, num2, num3, num4, res1, res2 =0.0;
-    char op1, op2, op3;
+    double num1, num2, num3, res = 0.0;
+    char op1, op2;
+    
     void input() {
         std::string str = "";
         std::cout << "Enter numbers with operators between: " << std::flush;
-        getline(std::cin, str);
+        std::cin >> num1 >> op1 >> num2 >> op2 >> num3;
         
-        // remove blank space from expression
-        int len = (int)str.length();
-        for (int i = 0; i < len; i++) {
-            if (str[i] == ' ' ) {
-                for (int j = i; j < len; j++)
-                    str[j] = str[j+1];
-                len--;
+        if ((op1 == '/' && num2 == 0) || (op2 == '/' && num3 == 0))
+            std::cout << "You can't divide by zero" << std::endl;
+        else if ((op2 == '*' || op2 == '/') && (op1 == '-' || op1 == '+')) {
+            switch (op2) {
+                case '*':
+                    res = num2*num3;
+                    break;
+                case '/':
+                    res = num2/num3;
+                    break;
+                case '%':
+                    res = (int)num2 % (int)num3;
+                    break;
+                default:
+                    break;
+            }
+            switch (op1) {
+                case '+':
+                    res = num1+res;
+                    break;
+                case '-':
+                    res = num1-res;
+                    break;
+                default:
+                    break;
             }
         }
-        
-        std::istringstream iss(str);
-        iss >> num1 >> op1 >> num2 >> op2 >> num3;
-    }
-    
-    
-    // Addition
-    void operator +() {
-        res1 = num1+num2;
-
-    }
-    // Subtraction
-    void operator -() {
-        res1 = num1-num2;
-
-    }
-    // Multiplication
-    void operator *() {
-        res1 = num1*num2;
+        else {
+            switch (op1) {
+                case '+':
+                    res = num1+num2;
+                    break;
+                case '-':
+                    res = num1-num2;
+                    break;
+                case '*':
+                    res = num1*num2;
+                    break;
+                case '/':
+                    res = num1/num2;
+                    break;
+                default:
+                    break;
+            }
+            switch (op2) {
+                case '+':
+                    res = res+num3;
+                    break;
+                case '-':
+                    res = res-num3;
+                    break;
+                case '*':
+                    res = res*num3;
+                    break;
+                case '/':
+                    res = res/num3;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
     
     void display() {
         if (num3 == '\0') {
-                    std::cout << "The answer to " << num1 << op1 << num2 << " is " << res1 << std::endl;
-
+            std::cout << "The answer to " << num1 << op1 << num2 << " is " << res << std::endl;
         }
-        else std::cout << "The answer to " << num1 << op1 << num2 << op2 << num3 << " is " << res2 << std::endl;
-
+        else std::cout << "The answer to " << num1 << op1 << num2 << op2 << num3 << " is " << res << std::endl;
     }
 };
 
-
 int main() {
-    calculator Calc1, Calc2;
-    Calc1.input();
-    
-    switch (Calc1.op1) {
-        case '+':
-            +Calc1;
-            break;
-        case '-':
-            -Calc1;
-            break;
-        case '*':
-            *Calc1;
-            break;
-            
-            //Division
-        case '/':
-            if (Calc1.num2 == 0) {
-                std::cout << "Not allowed" << std::endl;
-            }
-            Calc1.res1 = Calc1.num1/Calc1.num2;
-            break;
-            
-            // Modulus
-        case '%':
-            Calc1.res1 = (int)Calc1.num1 % (int)Calc1.num2;
-            break;
-        default:
-            break;
-    }
-    
-    switch (Calc1.op2) {
-        case '+':
-            Calc1.res2 = Calc1.res1 - Calc1.num3;
-            break;
-        case '-':
-             Calc1.res2 = Calc1.res1 - Calc1.num3;
-            break;
-        case '*':
-            Calc1.res2 = Calc1.res1 * Calc1.num3;
-            break;
-    }
-    Calc1.display();   
+    calculator Calc;
+    Calc.input();
+    Calc.display();
     return 0;
 }
