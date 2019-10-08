4e472b564d926fb197bba707573d381c087a08a2
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-08 15:26:24 +0200

    Update and rename w01_partD to w01_partD.cpp

---
+++ w01_partD.cpp
@@ -0,0 +1,9 @@
+/*
+ Part D is an extension of Part C. Create a program that reads a text string with multiple numbers and operators and prints the result. But now it should be possible, among other things, to use () around partial expressions and the program must deal with arbitrary number of parameters. It is also advantageous to add some other operators %, ^ (power) etc.
+ 
+ A natural extension is also to create the program so that the text string can be entered at the start of the program from the command line.
+ 
+ Example (The text at the back of the > is read during execution):
+ 
+ Enter two numbers with an operator between> 4 + 5 + (7 + 4) * 3 + 4 * 4 + 9/3 + 4 * 12 - 3/4
+ The answer to 4 + 5 + (7 + 4) * 3 + 4 * 4 + 9/3 + 4 * 12 - 3/4 is 108.2
