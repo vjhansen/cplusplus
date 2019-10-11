59db2c05b546ed1db0d6f3263431025eff87cbe3
Victor Hansen <0vjhansen@gmail.com>
2019-10-11 23:59:34 +0200

    12.10

--- tsp_ass1/tsp_ass1/main.cpp
+++ tsp_ass1/tsp_ass1/main.cpp
@@ -5,17 +5,19 @@
 
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <cmath>
 #include <algorithm>
+#include <chrono>
 
 //---------------------Point.h---------------------------------
 class Point {
 public:
     Point();  //-Default constructor
-    Point(float, float, float); //-Overloaded constructor
+    Point(double, double, double); //-Overloaded constructor
     
     ~Point(); //-Destructor
     
@@ -30,12 +32,12 @@ public:
     void setZ_coord();     //-modify z coordinate
     
     void printData();
-    double distanceTo(const Point &p) const;
+    double euclidDist(const Point &p) const;
     
 private:
-    float x_coord;
-    float y_coord;
-    float z_coord;
+    double x_coord;
+    double y_coord;
+    double z_coord;
 };
 //------------------------------------------------------
 
@@ -46,7 +48,7 @@ Point::Point() {
     z_coord = 0;
 }
 //-Overloaded constructor
-Point::Point(float x, float y, float z) {
+Point::Point(double x, double y, double z) {
     x_coord = x;
     y_coord = y;
     z_coord = z;
@@ -60,40 +62,59 @@ int Point::getX_coord() const {return x_coord;}
 int Point::getY_coord() const {return y_coord;}
 int Point::getZ_coord() const {return z_coord;}
 
-//-----------Set----------------------
-void fillVec(std::vector<Point>& new_vec_points) {
+//-3D Euclidean space
+double Point::euclidDist(const Point &p) const {
+    double x1, y1, z1;
+    double x2, y2, z2;
+    x1 = x_coord;
+    y1 = y_coord;
+    z1 = z_coord;
+    x2 = p.getX_coord();
+    y2 = p.getY_coord();
+    z2 = p.getZ_coord();
+    // sqrt( (x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2 )
+    return sqrt(pow(x2-x1,2) + pow(y2-y1,2) + pow(z2-z1,2));
+}
+
+//-Reading .txt-file containing points
+static void read_txt_and_fill(std::vector<Point>& vec_points) {
+    std::ifstream myfile;
+    std::vector<int> read_point;
+    myfile.open("test-10.txt");
+    if (myfile.is_open()) {
+        int points;
+        while (myfile>>points) {
+            read_point.push_back(points);
+        }
+        myfile.close();
+    }
+    else std::cout << "Unable to open file.";
     
-    float x,y,z;
-    std::cout<<"How many points(x,y,z)? ";
+    int n = 1;
+    // read_point.front() = number of points, = 5 for test-5.txt
+    for (int i=0; i<read_point.front(); i++) {
+        Point new_Points(read_point.at(n),read_point.at(n+1),read_point.at(n+2));
+        vec_points.push_back(new_Points);
+        n += 3; // needed to place points into x, y and z.
+    }
+}
+
+void manualFill(std::vector<Point>& new_vec_points) {
+    double x,y,z;
+    std::cout<<"\nHow many points? ";
     int size;
     std::cin>>size;
-    
     for (int i=0; i<size; i++) {
         std::cout<<"Point("<<i<<"): ";
         std::cin>>x>>y>>z;
-        std::cout<<std::endl;
         Point new_Points(x,y,z);
         new_vec_points.push_back(new_Points);
     }
 }
 
-double Point::distanceTo(const Point &p) const {
-    float x1, y1, z1;
-    float x2, y2, z2;
-    
-    x1 = x_coord;
-    y1 = y_coord;
-    z1 = z_coord;
-
-    x2 = p.getX_coord();
-    y2 = p.getY_coord();
-    z2 = p.getZ_coord();
-        
-    return sqrt( pow(x2-x1,2)+pow(y2-y1,2)+pow(z2-z1,2) );
-}
-
-// a->b =(*a).b, access member through pointer
+//-Print points
 void printVec(const std::vector<Point>& new_points) {
+    std::cout<<"\nWe have "<<new_points.size()<<" points\n";
     for (int i=0; i<new_points.size(); i++) {
         std::cout<<"Point("<<i<<"): "<<new_points[i].getX_coord()
         <<", "<<new_points[i].getY_coord()
@@ -102,93 +123,81 @@ void printVec(const std::vector<Point>& new_points) {
 }
 
 
-static void read_txt_file() {
-    std::string line;
-    std::ifstream myfile;
-    myfile.open("test-2.txt");
-    // fil inneholder antall points på linje 1
-    // videre inneholder den xyz-coord for hvert punkt.
-    
-    if (myfile.is_open())
-    {
-        while ( myfile.good() )
-        {
-            getline (myfile,line);
-            std::cout << line << std::endl;
-        }
-        myfile.close();
-    }
-    
-    else std::cout << "Unable to open file";
-}
-
-
-/* if the vector 'order' = [3 1 2 0], the total distance will be the sum of:
- - D{3:1} = distance from points[3] to points[1]
- - D{1:2}
- - D{2:0}
- - D{0:3} -- roundtrip
- 
- --> D_tot = D{3:1}+D{1:2}+D{2:0}+D{0:3}
- */
-
+/*  Function takes a vector of points, and the order in which those points are visited.
+    Then computes total length of path when the points are traveled in a specified order.
+    The path is a complete round trip */
 double circuitLength(const std::vector<Point>& points, const std::vector<int>& order) {
-    float d = 0.0;
-    for (int i = 0; i < order.size()-1; i++) {
-        d += points[order[i]].distanceTo(points[order[i+1]]);
+    double D_tot = 0.0; // total distance
+    std::vector<int>::const_iterator iter = order.begin();
+    Point start = points[*iter];    // Store starting value for roundtrip
+    Point current = points[*iter];  // Placeholder value
+    //iter++;
+    while (iter != order.end()) {
+        D_tot += points[*iter].euclidDist(current); // similar to points[order[i]].euclidDist(points[order[i+1]]);
+        current = points[*iter];
+        iter++;
     }
-    d += points[order.size() - 1].distanceTo(points[order[0]]);
-    return d;
+    // distance from end point back to starting point
+    D_tot += current.euclidDist(start);
+    return D_tot;
 }
 
-// use permutation
-std::vector<int> findShortestPath(const std::vector<Point>& points){
-    
+/*  Function takes a vector of points, and returns a vector that specifies the order to visit all of the points in a single round trip.
+    Visiting each point once, to ensure that the trip is as short as possible.
+    This is the function that will solve the TSP. */
+//most demanding function
+std::vector<int> findShortestPath(const std::vector<Point>& points) {
     std::vector<int> bestPath;
-    std::vector<int> path(points.size());
-    double short_Dist, curr_Dist;
-    
-    for (int i=0; i<path.size(); i++) {
-        path[i] = i;
+    std::vector<int> curr_path(points.size());
+    double shortest_Dist;
+
+    for (int i=0; i<curr_path.size(); i++) {
+        curr_path[i] = i;
     }
-    
-    short_Dist = circuitLength(points, path);
-    bestPath = path;
-    
-    while (std::next_permutation(path.begin(),path.end())) {
-        curr_Dist = circuitLength(points, path);
-        
-        if (curr_Dist < short_Dist) {
-            short_Dist = curr_Dist;
-            bestPath = path;
+    shortest_Dist = circuitLength(points, curr_path); // calculate reference distance in chronological order [0 1 2 n]
+    bestPath = curr_path;
+    while (std::next_permutation(curr_path.begin(),curr_path.end())) {
+        if (circuitLength(points, curr_path) < shortest_Dist) {
+            shortest_Dist = circuitLength(points, curr_path);
+            bestPath = curr_path;
         }
     }
     return bestPath;
 }
 
-
 static void printPath(std::vector<int> &shortestPath) {
-    std::cout <<"Best order: ";
+    std::cout <<"\nBest order: [ ";
     for (int i = 0; i < shortestPath.size(); i++) {
         std::cout << shortestPath.at(i) << ' ';
     }
+    std::cout<<"]";
 }
 
 int main() {
-    
     std::vector<Point> points;
-    //points.reserve(3);
-    fillVec(points);
+    int choice = 0;
+    std::cout<<"1. Read points from .txt-file.\n";
+    std::cout<<"2. Fill in points manually.\n";
+    std::cout<<"Your choice: ";
+    std::cin>>choice;
+    if (choice == 1) {
+        std::cout<<"reading from .txt-file\n";
+        read_txt_and_fill(points);
+    } else if (choice == 2) {
+        manualFill(points);
+    }
     printVec(points);
-    
     std::vector<int> shortestPath;
+    
+    auto start = std::chrono::high_resolution_clock::now();
     shortestPath = findShortestPath(points);
     printPath(shortestPath);
-    
     std::cout<<"\nShortest distance: "<< circuitLength(points, shortestPath)<<std::endl;
     
-    //read_txt_file();
-    points.clear();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Computation time: "<< duration.count() << " microseconds" << std::endl;
     
+    points.clear();
     return 0;
 }
