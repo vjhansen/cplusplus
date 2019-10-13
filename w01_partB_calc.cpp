22d6db91b9ab4f3a8dea9723b049227f444dce4b
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-13 11:29:31 +0200

    Update w01_partB_calc.cpp

--- w01_partB_calc.cpp
+++ w01_partB_calc.cpp
@@ -34,7 +34,7 @@ int main() {
             break;
         case '/':
             if (num2 == 0) { // division by zero
-                cout << "сука блять";
+                cout << "Don't divide by zero!";
             }
             else
                 cout<<endl<< "The answer to " << num1 << op << num2 << " is " << (double) num1/num2 << endl;
