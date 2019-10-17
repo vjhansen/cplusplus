0799a8a2ae8785aa99ae9551bfbe9d21a6447ddd
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-17 14:39:42 +0200

    Update w03_part2_inheritance.cpp

--- w03_part2_inheritance.cpp
+++ w03_part2_inheritance.cpp
@@ -1,16 +1,18 @@
 /*
  W03 - assignment 2
  1. Create a class SecretAgent where the access to class Human is private.
- 
  2.Add the data number (007 or similar) include this get and set methods, verify that these can be used in the main(), while the others can not.
  */
-
 
 #include <iostream>
 #include <string>
 
 //------------------------------------
 class Human {
+private:
+    std::string Name;
+    int Age;
+ 
 public:
     Human() {
         std::cout<<"Creating default Human"<<std::endl;
@@ -34,19 +36,16 @@ public:
         int a = 0;
         Age = a;
     }
-    
     virtual void printHuman() {
         std::cout<<"Name: "<<Name<<std::endl <<"Age: "<< Age << std::endl;
     }
-    
-private:
-    std::string Name;
-    int Age;
 };
 
 class SecretAgent : private Human {
-public:
-    
+private:
+    std::string agentNr;
+
+public:    
     SecretAgent(){
     //Name = " "; //- private member of human
     //Age = 0;    //- private member of human
@@ -65,23 +64,18 @@ public:
     void setAgentNr() {
         std::string a_nr = "007";
         agentNr = a_nr;
-    }
-    
+    }    
     void printAgent() {
         Human::printHuman();
         std::cout<<"Agent: "<<agentNr<<std::endl;
     }
-    
-private:
-    std::string agentNr;
 };
-
 //------------------------------------
 int main() {
     SecretAgent james;
     james.setAgentNr();
     james.printAgent();
-    
+   
     SecretAgent S("Stalin",40,"008");
     S.printAgent();
     return 0;   
