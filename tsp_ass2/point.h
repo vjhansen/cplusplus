4613efebf6dc94ca73eb7381e2278d98b28973cd
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-25 21:53:33 +0200

    Update point.h

--- tsp_ass2/point.h
+++ tsp_ass2/point.h
@@ -1,31 +1,10 @@
+//
+//
 
 #ifndef POINT_H
 #define POINT_H
 
-
-// Curiously recurring template pattern
-template <typename T>
-struct counter {
-    static int Points_created;
-    static int Points_alive;
-    counter() {
-        ++Points_created;
-        ++Points_alive;
-    }
-    counter(const counter&) {
-        ++Points_created;
-        ++Points_alive;
-    }
-protected: //Protected members are accessible in the class that defines them and in classes that inherit from that class.
-    ~counter(){
-        --Points_alive;
-    }
-};
-template <typename T> int counter<T>::Points_created(0);
-template <typename T> int counter<T>::Points_alive(0);
-
-
-class Point : counter<Point> {
+class Point{
 private:
     double x_coord;
     double y_coord;
@@ -41,14 +20,9 @@ public:
     int getX_coord() const {return x_coord;}    //-return x coordinate
     int getY_coord() const {return y_coord;}    //-return y coordinate
     int getZ_coord() const {return z_coord;}    //-return z coordinate
-
-    //-Mutators (modifiable)
-    void setX_coord();     //-modify x coordinate
-    void setY_coord();     //-modify y coordinate
-    void setZ_coord();     //-modify z coordinate
     
-    void printData();
-    double calcDist(const Point &p) const;
+    void printData(); //
+    double calcDist(const Point& p) const; //
 };
 
 #endif
