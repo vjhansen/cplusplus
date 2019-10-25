03e4b39e5858152a20c1bad2da5b9a39797f1842
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-10-25 21:50:55 +0200

    Update main.cpp

--- tsp_ass2/main.cpp
+++ tsp_ass2/main.cpp
@@ -2,12 +2,13 @@
 //  OOP4200
 //  Travelling salesman problem, v2
 //  Genetic algorithms (GAs)
-//  Victor J. Hansen, 2019
+//  Victor J. Hansen, October 2019
 
 //  clang++ -g -std=c++11 -stdlib=libc++ -Wall main.cpp tsp-ga.cpp
 
+
+// sjekk hvilke av disse som trengs
 #include <iostream>
-#include <sstream>
 #include <fstream>
 #include <vector>
 #include <string>
@@ -17,15 +18,12 @@
 #include <ctime>
 #include <set>
 
-#include "point.h" //Point class
-#include "tsp-ga.h"
+#include "point.h"  // Point class
+#include "tsp-ga.h" //
+
 
 //-Overloaded constructor
-Point::Point(double x, double y, double z) {
-    x_coord = x;
-    y_coord = y;
-    z_coord = z;
-}
+Point::Point(double x, double y, double z): x_coord(x), y_coord(y), z_coord(z){}
 
 //-3D Euclidean space: sqrt( (x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2 )
 double Point::calcDist(const Point& p2) const {
@@ -38,8 +36,13 @@ double Point::calcDist(const Point& p2) const {
 static void read_txt_and_fill(std::vector<Point>& txt_points) {
     std::ifstream myfile;
     std::vector<int> read_point;
-    myfile.open("test-60.txt");
+    std::cout<<"\nHow many points?\n(2, 5, 8, 10, 12, 30, 60, 100, 300, 500): ";
+    int size;
+    std::cin>>size;
+    std::string filestr ="test-txt/test-"+std::to_string(size)+".txt"; 
+    myfile.open(filestr);
     if (myfile.is_open()) {
+    	std::cout<<"\n** reading from .txt-file **\n";
         int points;
         while (myfile>>points) {
             read_point.push_back(points);
@@ -52,24 +55,10 @@ static void read_txt_and_fill(std::vector<Point>& txt_points) {
     for (int i=0; i<read_point.front(); i++) {
         Point new_Points(read_point.at(n),read_point.at(n+1),read_point.at(n+2));
         txt_points.push_back(new_Points);
-        n +=3; // needed to place points into x, y and z.
+        n+=3; // needed to insert points into x, y and z.
     }
 }
 
-void manualFill(std::vector<Point>& fill_points) {
-    double x,y,z;
-    std::cout<<"\nHow many points? ";
-    int size;
-    std::cin>>size;
-    for (int i=0; i<size; i++) {
-        std::cout<<"Point("<<i<<"): ";
-        std::cin >> x >> y >> z;
-        Point new_Points(x,y,z);
-        fill_points.push_back(new_Points);
-    }
-}
-
-//-Print points
 void printVec(const std::vector<Point>& points) {
     std::cout<<"\nWe have "<<points.size()<<" points\n";
    for (int i=0; i<points.size(); i++) {
@@ -87,29 +76,21 @@ void printVec(const std::vector<Point>& points) {
     std::cout<<shortestPath[shortestPath.size()-1]<<"]"<<std::endl;
 }
 
-void PrintNumberOfPoints(const std::vector<Point>& points) {
-    std::cout<<"\nNumber of Point objects created = "<<counter<Point>::Points_created;
-    std::cout<<"\nNumber of Point objects alive = "<<counter<Point>::Points_alive<<std::endl;
-}
-
-
 int main(int argc, char **argv) {
-    //int choice = 0;
     std::vector<Point> points;
-    std::cout<<"\n** reading from .txt-file **\n";
     read_txt_and_fill(points);
-    if (points.size() < 60) { // not necessary to print too many points
+    if (points.size() < 60) {
         printVec(points);
     }
     else std::cout<<"Read "<<points.size()<<" points"<<std::endl;
-    
-    // = points.size();
+
     unsigned int popSize;   // positive int
     unsigned int numGen;    // positive int
     float keepPop;          // float [0, 1] % of population to keep
     float numMut;           // positive float, e.g. 1.5*pop
 
     //https://www.cs.swarthmore.edu/~newhall/unixhelp/C_commandlineargs.php
+
     /* popSize = (int) atoi(argv[1]);
     int numGen = (int) atoi(argv[2]);
     int keepPop = (int) ( atof(argv[3]) * ((double)popSize + 0.5 ));
@@ -119,14 +100,14 @@ int main(int argc, char **argv) {
     std::cin>>popSize;
     std::cout<< "\nEnter number of generations: ";
     std::cin>>numGen;
-    std::cout<< "\nKeep (0-1): ";
+    std::cout<< "\nKeep (float)[0, 1]: ";
     std::cin>>keepPop;
     std::cout<< "\nMutation (float): ";
     std::cin>>numMut;
 
     if (popSize<0||numGen<0||keepPop<0||keepPop>1||numMut<0) {
         std::cout<<"**Values out of range**"<<std::endl;
-        return 0;
+        exit(1);
     }
     numMut = (int)popSize*numMut;
     keepPop =  (int)(double(popSize+0.5))*keepPop;
